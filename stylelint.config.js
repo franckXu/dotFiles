0ee8f3f47436de46023c0dd5cf0a016832b58273
man xu <xym>
2018-09-05 17:53:06 +0800

    chore

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: "stylelint-config-standard",
+    rules: {
+        "at-rule-empty-line-before": null,
+        "rule-empty-line-before": null,
+        indentation: 4
+    }
+};
